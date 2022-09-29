290655d3b4fd72e2956123969ba6e2ddb5846042
Bautista Pietraroia <bautistapietraroia@gmail.com>
2022-09-28 22:30:37 -0300

    fixed button color

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ let segundoNum;
 let cuenta = ""
 let resultadoFinal;
 let display;
+let buttonColor = "#e3e0e8"
 
 // Reset button color
 
 const resetColor = () => {
-    document.querySelectorAll('.cuenta').forEach(elem => elem.style.background = "#e3e0e8");
+    document.querySelectorAll('.cuenta').forEach(elem => elem.style.background = buttonColor)
 }
 
 // Display account
@@ -162,26 +163,31 @@ const light = document.querySelector('#light').addEventListener('click', () => {
     document.querySelectorAll('.boton').forEach(elem => elem.style.background = "#e3e0e8");
     document.querySelector('#pantalla').style.background = "#e3e0e8"
     document.querySelector('.calculadora').style.background = "#f4f1f9"
+    buttonColor = "#e3e0e8"
 })
 const dark = document.querySelector('#dark').addEventListener('click', () => {
     document.querySelectorAll('.boton').forEach(elem => elem.style.background = "#2d2f2f");
     document.querySelector('#pantalla').style.background = "#2d2f2f"
     document.querySelector('.calculadora').style.background = "#515555"
+    buttonColor = "#2d2f2f"
 })
 const purple = document.querySelector('#purple').addEventListener('click', () => {
     document.querySelectorAll('.boton').forEach(elem => elem.style.background = "hotpink");
     document.querySelector('#pantalla').style.background = "#e3e0e8"
     document.querySelector('.calculadora').style.background = "#f4f1f9"
+    buttonColor = "hotpink"
 })
 const orange = document.querySelector('#orange').addEventListener('click', () => {
     document.querySelectorAll('.boton').forEach(elem => elem.style.background = "coral");
     document.querySelector('#pantalla').style.background = "#e3e0e8"
     document.querySelector('.calculadora').style.background = "#f4f1f9"
+    buttonColor = "coral"
 })
 const blue = document.querySelector('#blue').addEventListener('click', () => {
     document.querySelectorAll('.boton').forEach(elem => elem.style.background = "aquamarine");
     document.querySelector('#pantalla').style.background = "#e3e0e8"
     document.querySelector('.calculadora').style.background = "#f4f1f9"
+    buttonColor = "aquamarine"
 })
 
 
